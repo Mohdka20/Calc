1e434c169a64a317f289f94ddd7dcf36abdcec98
mohdka20 <kaloty.mo@gmail.com>
2021-12-20 19:05:53 -0500

    First things first

---
+++ src/Calculator.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener  {
+	// declarations 
+	double input1 = 0;
+	double input2 = 0;
+	double result = 0;
+	char function ;
+	
+	JFrame frame;
+	JTextField field;
+	JButton [] numInput = new JButton [10];
+	JButton [] funInput = new JButton [8];
+	JButton add,sub,div,mult,eq,dot,cls,del;
+
+	
+	
+	
+	Calculator(){
+		 frame = new JFrame ("Calculator"); 
+		 frame.setSize(450,550);
+		 frame.setVisible(true);
+		 frame.setLayout(null);
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	
+	}
+	public static void main(String[] args) {
+	
+
+	
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+}
